853901b0f3a53a78e3d5cd5b590de22799afab47
higashi000 <higashi.ict@gmail.com>
2018-12-01 18:35:41 +0900

    [change] point Evaluation

--- front-end/Acceleration.js
+++ front-end/Acceleration.js
@@ -6,7 +6,7 @@ function loopFunc() {
 }
 function lastFunc() {
   // ループの終わりに実行する処理
-  alert(Math.ceil(playMovePoint));
+  alert(Math.ceil(playMovePoint * 10));
 }
 var loopTimes = 10; // ループする回数
 var loopInterval = 1000; // ループする間隔(ミリ秒)
