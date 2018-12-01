ee8846e876a665554b5da38cfcb8dda0fae4fd2c
higashi000 <higashi.ict@gmail.com>
2018-12-01 16:18:20 +0900

    [add] ginnnan

--- front-end/Acceleration.js
+++ front-end/Acceleration.js
@@ -10,7 +10,7 @@ function loopFunc() {
 }
 function lastFunc() {
   // ループの終わりに実行する処理
-  alert(playMovePoint);
+  alert('マジ' + playMovePoint + playMovePoint);
 }
 var loopTimes = 10; // ループする回数
 var loopInterval = 1000; // ループする間隔(ミリ秒)
