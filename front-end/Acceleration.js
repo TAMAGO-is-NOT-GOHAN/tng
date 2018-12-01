5626ce4b0ad822e223e23b6f4450dc1747a114fc
higashi000 <higashi.ict@gmail.com>
2018-12-01 13:51:16 +0900

    [add] get move point

--- front-end/Acceleration.js
+++ front-end/Acceleration.js
@@ -7,24 +7,35 @@ aY[1] = 0;
 var aZ = new Array(2);
 aZ[0] = 0;
 aZ[1] = 0;
-window.addEventListener("devicemotion", (dat) => {
-  aX[0] = dat.accelerationIncludingGravity.x;
-  aY[0] = dat.accelerationIncludingGravity.y;
-  aZ[0] = dat.accelerationIncludingGravity.z;
-});
+
+var playMovePoint = 0;
 
 var timer = window.setInterval(() => {
-  dispAlert();
-}, 1000);
+  main();
+}, 10000);
 
-window.addEventListener("devicemotion", (dat) => {
-  aX[1] = dat.accelerationIncludingGravity.x;
-  aY[1] = dat.accelerationIncludingGravity.y;
-  aZ[1] = dat.accelerationIncludingGravity.z;
-});
+alert(playMovePoint);
 
-function dispAlert() {
-//  if (aX[1] > aX[0] + 5 && aY[1] > aY[0] + 5 && aZ[1] > aZ[0] + 5) {
-    alert('めっちゃ動いてそう(小並)');
-//  }
+function main() {
+  window.addEventListener("devicemotion", (dat) => {
+    aX[0] = dat.accelerationIncludingGravity.x;
+    aY[0] = dat.accelerationIncludingGravity.y;
+    aZ[0] = dat.accelerationIncludingGravity.z;
+  });
+
+  var timer = window.setInterval(() => {}, 100);
+
+  window.addEventListener("devicemotion", (dat) => {
+    aX[1] = dat.accelerationIncludingGravity.x;
+    aY[1] = dat.accelerationIncludingGravity.y;
+    aZ[1] = dat.accelerationIncludingGravity.z;
+  });
+
+  playMovePoint += abs(aX[1] - aX[0]);
+  playMovePoint += abs(aY[1] - aY[0]);
+  playMovePoint += abs(aZ[1] - aZ[0]);
 }
+
+function abs(var value) {
+  return value > 0 ? value : -value;
+};
