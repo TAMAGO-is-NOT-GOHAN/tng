3d2f400fd1ddab7bcdef7e18f8df60ce1b36d2b7
higashi000 <higashi.ict@gmail.com>
2018-12-01 16:46:24 +0900

    [fix] main func

--- front-end/Acceleration.js
+++ front-end/Acceleration.js
@@ -2,7 +2,36 @@ var playMovePoint = 0.0;
 
 function loopFunc() {
   // ループする処理
-  main();
+  var aX1, aX2;
+  var aY1, aY2;
+  var aZ1, aZ2;
+  //最初の加速度を取得
+  window.addEventListener("devicemotion", (dat) => {
+    aX1 = dat.accelerationIncludingGravity.x;
+    aY1 = dat.accelerationIncludingGravity.y;
+    aZ1 = dat.accelerationIncludingGravity.z;
+    document.write(dat.accelerationIncludingGravity.x + ", " + aX1);
+    document.write(dat.accelerationIncludingGravity.y + ", " + aY1);
+    document.write(dat.accelerationIncludingGravity.z + ", " + aZ1);
+  });
+
+  //最初の加速度を取得
+  sleep(100);
+
+  //0.1秒後の加速度を取得
+  window.addEventListener("devicemotion", (dat) => {
+    aX2 = dat.accelerationIncludingGravity.x;
+    aY2 = dat.accelerationIncludingGravity.y;
+    aZ2 = dat.accelerationIncludingGravity.z;
+    document.write(dat.accelerationIncludingGravity.x + ", " + aX2);
+    document.write(dat.accelerationIncludingGravity.y + ", " + aY2);
+    document.write(dat.accelerationIncludingGravity.z + ", " + aZ2);
+  });
+
+  //playMovePointに最初と0.1秒後の加速度の絶対値を代入
+  playMovePoint += aX2 - aX1;
+  playMovePoint += aY2 - aY1;
+  playMovePoint += aZ2 - aZ1;
 }
 function lastFunc() {
   // ループの終わりに実行する処理
@@ -32,6 +61,7 @@ function sleep(waitMsec) {
   while (new Date() - startMsec < waitMsec);
 }
 
+/*
 function main() {
   var aX1, aX2;
   var aY1, aY2;
@@ -68,6 +98,7 @@ function main() {
   //document.write(aY[1] + ", " + aY[0]);
   //document.write(aZ[1] + ", " + aZ[0]);
 }
+*/
 
 // function abs(var value) {
 //   return value > 0 ? value : value * -1;
