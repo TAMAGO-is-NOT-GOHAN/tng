265eb2921ca237a12d5c146f33d65d585a5f450b
itok01 <itok01tech@gmail.com>
2018-12-02 08:41:44 +0900

    [add] point.js

---
+++ point.js
@@ -0,0 +1,16 @@
+var param = location.search
+
+var username = getParam('username')
+document.getElementById("username").innerHTML = username;
+var score = getParam('score')
+document.getElementById("score").innerHTML = score;
+
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
