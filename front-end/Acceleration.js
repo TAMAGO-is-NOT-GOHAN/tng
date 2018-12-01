5e7ef718f0bdf99e3e6146398199d74bb4af4455
higashi000 <higashi.ict@gmail.com>
2018-12-01 18:05:16 +0900

    [fix] fix

--- front-end/Acceleration.js
+++ front-end/Acceleration.js
@@ -38,9 +38,9 @@ function main() {
   var aZ1, aZ2;
   //最初の加速度を取得
   window.addEventListener("devicemotion", (dat) => {
-    aX1 = dat.accelerationIncludingGravity.x;
-    aY1 = dat.accelerationIncludingGravity.y;
-    aZ1 = dat.accelerationIncludingGravity.z;
+    playMovePoint += dat.accelerationIncludingGravity.x;
+    playMovePoint += dat.accelerationIncludingGravity.y;
+    playMovePoint += dat.accelerationIncludingGravity.z;
     document.write(dat.accelerationIncludingGravity.x + ", " + aX1);
     document.write(dat.accelerationIncludingGravity.y + ", " + aY1);
     document.write(dat.accelerationIncludingGravity.z + ", " + aZ1);
@@ -51,18 +51,15 @@ function main() {
 
   //0.1秒後の加速度を取得
   window.addEventListener("devicemotion", (dat) => {
-    aX2 = dat.accelerationIncludingGravity.x;
-    aY2 = dat.accelerationIncludingGravity.y;
-    aZ2 = dat.accelerationIncludingGravity.z;
+    playMovePoint += dat.accelerationIncludingGravity.x;
+    playMovePoint += dat.accelerationIncludingGravity.y;
+    playMovePoint += dat.accelerationIncludingGravity.z;
     document.write(dat.accelerationIncludingGravity.x + ", " + aX2);
     document.write(dat.accelerationIncludingGravity.y + ", " + aY2);
     document.write(dat.accelerationIncludingGravity.z + ", " + aZ2);
   });
 
   //playMovePointに最初と0.1秒後の加速度の絶対値を代入
-  playMovePoint += aX2 - aX1;
-  playMovePoint += aY2 - aY1;
-  playMovePoint += aZ2 - aZ1;
 
   //document.write(aX[1] + ", " + aX[0]);
   //document.write(aY[1] + ", " + aY[0]);
