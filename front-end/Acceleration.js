47222ccd47719a6543b06faefc6830b16f02cfb6
higashi000 <higashi.ict@gmail.com>
2018-12-01 21:14:13 +0900

    [remove] vibrate func

--- front-end/Acceleration.js
+++ front-end/Acceleration.js
@@ -6,7 +6,6 @@ function loopFunc() {
 }
 function lastFunc() {
   // ループの終わりに実行する処理
-  navigator.vibrate([500, 100, 500]);
   alert(Math.ceil(playMovePoint * 10));
 }
 var loopTimes = 10; // ループする回数
