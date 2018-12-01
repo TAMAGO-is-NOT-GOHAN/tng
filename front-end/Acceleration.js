5f6631ee774d8c0e59bb2effc0ddde0015278a6a
higashi000 <higashi.ict@gmail.com>
2018-12-01 12:08:56 +0900

    [add] disp alert

--- front-end/Acceleration.js
+++ front-end/Acceleration.js
@@ -6,13 +6,16 @@ window.addEventListener("devicemotion", (dat) => {
   aZ[0] = dat.accelerationIncludingGravity.z;
 });
 
-var timer = window.setInterval(() => {
-  displayData();
-}, 1000);
+var timer = window.setInterval(() => {}, 1000);
 
-function displayData() {
-  var txt = document.getElementById("txt");
-  txt.innerHTML = "x: " + aX + "<br>"
-                + "y: " + aY + "<br>"
-                + "z: " + aZ;
+window.addEventListener("devicemotion", (dat) => {
+  aX[1] = dat.accelerationIncludingGravity.x;
+  aY[1] = dat.accelerationIncludingGravity.y;
+  aZ[1] = dat.accelerationIncludingGravity.z;
+});
+
+function phoneVib() {
+   if (aX[1] > aX[0] + 5 && aY[1] > aY[0] + 5 && aZ[1] > aZ[0] + 5) {
+     alert('めっちゃ動いてそう(小並)');
+   }
 }
