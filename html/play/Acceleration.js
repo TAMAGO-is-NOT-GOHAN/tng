e49d16de0bf245452235b8438113222b20a20a31
itok01 <itok01tech@gmail.com>
2019-08-19 13:32:29 +0900

    Move contents

---
+++ html/play/Acceleration.js
@@ -0,0 +1,75 @@
+// ユーザーネームの取得
+var param = location.search
+
+var username = getParam('username')
+
+function getParam(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var playMovePoint = 0.0;
+
+function loopFunc() {
+  // ループする処理
+  main();
+}
+function lastFunc() {
+  // ループの終わりに実行する処理
+  // alert(Math.ceil(playMovePoint / 100));
+  location.href = "../result/index.html?username=" + username + "&score=" + Math.ceil(playMovePoint / 100);
+  navigator.vibrate([1000]);
+}
+var loopTimes = 20; // ループする回数
+var loopInterval = 1000; // ループする間隔(ミリ秒)
+
+var count = 0;
+var countup = function () {
+  console.log(count++);
+}
+
+var loop = setInterval(function () {
+  loopFunc();
+  countup();
+  if (count >= loopTimes) {
+    lastFunc();
+    clearInterval(loop);
+  }
+}, loopInterval);
+
+function sleep(waitMsec) {
+  var startMsec = new Date();
+
+  while (new Date() - startMsec < waitMsec);
+}
+
+function main() {
+  var aX1, aX2;
+  var aY1, aY2;
+  var aZ1, aZ2;
+  //最初の加速度を取得
+  window.addEventListener("devicemotion", (dat) => {
+    playMovePoint += abs(dat.accelerationIncludingGravity.x);
+    playMovePoint += abs(dat.accelerationIncludingGravity.y);
+    playMovePoint += abs(dat.accelerationIncludingGravity.z);
+  });
+
+  //最初の加速度を取得
+  sleep(50);
+
+  window.addEventListener("devicemotion", (dat) => {
+    //0.1秒後の加速度を取得
+    playMovePoint += abs(dat.accelerationIncludingGravity.x);
+    playMovePoint += abs(dat.accelerationIncludingGravity.y);
+    playMovePoint += abs(dat.accelerationIncludingGravity.z);
+  });
+}
+
+function abs(value) {
+  return value > 0 ? value : value * -1;
+}
---
+++ html/result/point.js
@@ -0,0 +1,77 @@
+var param = location.search
+
+var username = getParam('username')
+document.getElementById("username").innerHTML = username;
+var score = getParam('score')
+document.getElementById("score").innerHTML = score;
+
+function getParam(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function getCSVFile() {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    createArray(xhr.responseText);
+  };
+
+  xhr.open("get", "data.csv", true);
+  xhr.send(null);
+}
+getCSVFile();
+
+function createXMLHttpRequest() {
+  var XMLhttpObject = null;
+  XMLhttpObject = new XMLHttpRequest();
+  return XMLhttpObject;
+}
+
+function createArray(csvData) {
+  var tempArray = csvData.split("\n");
+  var csvArray = new Array();
+  for(var i = 0; i < tempArray.length; i++){
+    csvArray[i] = tempArray[i].split(",");
+  }
+  console.log(csvArray);
+}
+
+// ファイル操作
+window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+
+function onError () { console.log ('Error : ', arguments); }
+
+navigator.webkitPersistentStorage.requestQuota (1024*1024*1024, function(grantedBytes) {
+  console.log ('requestQuota: ', arguments);
+  requestFS(grantedBytes);
+}, onError);
+
+function requestFS(grantedBytes) {
+  window.webkitRequestFileSystem(window.PERSISTENT, grantedBytes, function(fs) {
+    console.log ('fs: ', arguments); // I see this on Chrome 27 in Ubuntu
+  }, onError);
+}
+
+fs.root.getFile('log.txt', {create: true}, function(fileEntry) {
+
+  // Create a FileWriter object for our FileEntry (log.txt).
+  fileEntry.createWriter(function(fileWriter) {
+
+    fileWriter.onwriteend = function(e) {
+      console.log('Write completed.');
+    };
+
+    fileWriter.onerror = function(e) {
+      console.log('Write failed: ' + e.toString());
+    };
+
+    // Create a new Blob and write it to log.txt.
+    var bb = new Blob(['Lorem Ipsum']);
+    fileWriter.write(bb);
+  }, onError);
+}, onError);
