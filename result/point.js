5b32b4bdcc52a849479b0c88f58d5eb534d8b2b5
itok01 <itok01tech@gmail.com>
2019-08-20 13:54:00 +0900

    Fix writing file

--- result/point.js
+++ result/point.js
@@ -26,12 +26,6 @@ function getCSVFile() {
 }
 getCSVFile();
 
-function createXMLHttpRequest() {
-  var XMLhttpObject = null;
-  XMLhttpObject = new XMLHttpRequest();
-  return XMLhttpObject;
-}
-
 function createArray(csvData) {
   var tempArray = csvData.split("\n");
   var csvArray = new Array();
@@ -41,42 +35,6 @@ function createArray(csvData) {
   console.log(csvArray);
 }
 
-// ファイル操作
-window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
-
-function onError() { console.log('Error : ', arguments); }
-
-navigator.webkitPersistentStorage.requestQuota(1024 * 1024 * 1024, function (grantedBytes) {
-  console.log('requestQuota: ', arguments);
-  requestFS(grantedBytes);
-}, onError);
-
-function requestFS(grantedBytes) {
-  window.webkitRequestFileSystem(window.PERSISTENT, grantedBytes, function (fs) {
-    console.log('fs: ', arguments); // I see this on Chrome 27 in Ubuntu
-  }, onError);
-}
-
-fs.root.getFile('log.txt', { create: true }, function (fileEntry) {
-
-  // Create a FileWriter object for our FileEntry (log.txt).
-  fileEntry.createWriter(function (fileWriter) {
-
-    fileWriter.onwriteend = function (e) {
-      console.log('Write completed.');
-    };
-
-    fileWriter.onerror = function (e) {
-      console.log('Write failed: ' + e.toString());
-    };
-
-    // Create a new Blob and write it to log.txt.
-    var bb = new Blob(['Lorem Ipsum']);
-    fileWriter.write(bb);
-  }, onError);
-}, onError);
-
-// https://twitter.com/intent/tweet?text=テキストテキスト
 function tweet() {
   window.open("https://twitter.com/intent/tweet?text=私は%20%23DynamicDance%20で" + score + "点を獲得しました！", "_blank");
 }
